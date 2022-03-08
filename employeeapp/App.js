5541e64bedcc62d9cdbfa2181aa5c44aa2ae5e3e
Gourab Banik <58955646+Gourab-hub@users.noreply.github.com>
2022-03-08 20:05:46 +0530

    11.Adding React Navigation V6

--- employeeapp/App.js
+++ employeeapp/App.js
@@ -5,25 +5,53 @@ import Home from './screens/Home';
 import CreateEmployy from './screens/CreateEmployee';
 import Profile from './screens/Profile';
 
+// Navigate 
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 
-export default function App() {
+const Stack = createStackNavigator();
+
+
+
+function App() {
   return (
     <View style={styles.container}>
       {/* <Home /> */}
       {/* <CreateEmployy/> */}
-      <Profile/>
-      {/* <Text>hii</Text> */}
-      <StatusBar style="auto" />
+
+      {/* <Profile /> */}
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Create" component={CreateEmployy}
+           options={{
+            title: 'Create Employee',
+          }} />
+        <Stack.Screen name="Profile" component={Profile} />
+
+      </Stack.Navigator>
+
     </View>
   );
 }
 
+
+
+//https://reactnavigation.org/docs/getting-started
+export default () => {
+  return (
+    <NavigationContainer>
+      <App />
+    </NavigationContainer>
+  )
+}
+
+
 const styles = StyleSheet.create({
-  container: { 
-     flex:1,
+  container: {
+    flex: 1,
     backgroundColor: '#eee',
-    marginTop: Constants.statusBarHeight,
+    // marginTop: Constants.statusBarHeight,
 
   },
 });
